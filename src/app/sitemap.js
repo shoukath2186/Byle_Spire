711d2278ec9fabfd111bfed348ee2b7f8f8ba2c6
shoukath <shoukathot77@gmail.com>
2025-10-17 11:40:57 +0530

    added site map

---
+++ src/app/sitemap.js
@@ -0,0 +1,49 @@
+// app/sitemap.xml/route.ts
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset 
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+<url>
+<loc>https://www.bytespire.in/</loc>
+<lastmod>2025-10-17T06:07:30+00:00</lastmod>
+<priority>1.00</priority>
+</url>
+<url>
+<loc>https://www.bytespire.in/services</loc>
+<lastmod>2025-10-17T06:07:30+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://www.bytespire.in/projects</loc>
+<lastmod>2025-10-17T06:07:30+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://www.bytespire.in/career</loc>
+<lastmod>2025-10-17T06:07:30+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://www.bytespire.in/about</loc>
+<lastmod>2025-10-17T06:07:30+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://www.bytespire.in/contact</loc>
+<lastmod>2025-10-17T06:07:30+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+
+</urlset>`;
+
+    return new NextResponse(xml, {
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+    });
+}
