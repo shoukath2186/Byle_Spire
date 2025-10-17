33cda52af22e79675fe6d8ca4743be4454b223b1
shoukath <shoukathot77@gmail.com>
2025-10-17 11:44:45 +0530

    added site map 2

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,8 +1,7 @@
-// app/sitemap.xml/route.ts
-import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server'
 
 export async function GET() {
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset 
   xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
@@ -41,9 +40,9 @@ export async function GET() {
 
 </urlset>`;
 
-    return new NextResponse(xml, {
-        headers: {
-            'Content-Type': 'application/xml',
-        },
-    });
+  return new NextResponse(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
 }
